a3068b21e2f74cca2950fda52045dba0c1b25fa9
Maya Karabula-Stysiak <maya.karabula@contentful.com>
2022-10-10 17:41:15 +0200

    try serverless

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use('/', express.static('docs'))
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
