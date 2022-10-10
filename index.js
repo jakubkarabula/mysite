4550a7d68bb3de2c7d31a4ac2f8c89e918f6aac8
Maya Karabula-Stysiak <maya.karabula@contentful.com>
2022-10-10 17:54:26 +0200

    test gemini

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var http = require('http')
+const gemini = require('gemini-server').default
+const readFileSync = require('fs').readFileSync
 
-module.exports = http.createServer((req, res) => {
-  res.end('hello')
+const options = {
+  cert: readFileSync('cert.pem'),
+  key: readFileSync('key.pem'),
+}
+
+const app = gemini(options)
+
+app.on('/', (_req, res) => {
+  res.file('gemini/index.gmi')
+})
+
+module.exports = app.listen(1965, () => {
+  console.log('Listening...')
 })
