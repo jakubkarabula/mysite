9967be17b5f30bafda1c58ef54093d47623278ea
Maya <maya.karabulastysiak@gmail.com>
2024-02-19 18:09:19 +0100

    enable https back

--- http-server.js
+++ http-server.js
@@ -3,15 +3,15 @@ var http = require('http');
 var https = require('https');
 const readFileSync = require('fs').readFileSync
 
-// const privateKey = readFileSync('/etc/letsencrypt/live/mayaks.eu/privkey.pem', 'utf8');
-// const certificate = readFileSync('/etc/letsencrypt/live/mayaks.eu/cert.pem', 'utf8');
-// const ca = readFileSync('/etc/letsencrypt/live/mayaks.eu/chain.pem', 'utf8');
+const privateKey = readFileSync('/etc/letsencrypt/live/mayaks.eu/privkey.pem', 'utf8');
+const certificate = readFileSync('/etc/letsencrypt/live/mayaks.eu/cert.pem', 'utf8');
+const ca = readFileSync('/etc/letsencrypt/live/mayaks.eu/chain.pem', 'utf8');
 
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca
-// };
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 
 const app = express()
 
@@ -20,7 +20,7 @@ app.use('/seis', express.static('seis'))
 app.use('/', express.static('public'))
 
 var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
+var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080, () => console.log('running http'));
-// httpsServer.listen(443, () => console.log('running https'));
+httpsServer.listen(443, () => console.log('running https'));
